38e29329742cbaee9ef40450abc6a5f686922377
rohitandprojects <rohitandprojects@gmail.com>
2025-02-12 18:24:37 +0530

    Added Dark Mode Functionality

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,27 +1,30 @@
 import Link from 'next/link'
 import React from 'react'
+import DarkModeSwitch from './DarkModeSwitch'
 
 const Header = () => {
   return (
     <div className='flex justify-between items-center p-3 max-auto max-w-full'>
         <ul className='flex gap-4'>
             <li>
-                <Link href={'/sign-in'}>Sign In</Link>                
+                <Link href={'/sign-in'}>Sign In</Link>
             </li>
             <li className='hidden sm:block'>
-                <Link href={'/'}>Home</Link>                
+                <Link href={'/'}>Home</Link>
             </li>
             <li className='hidden sm:block'>
-                <Link href={'/about'}>About</Link>                
+                <Link href={'/about'}>About</Link>
             </li>
-
         </ul>
-    <Link href={'/'} className='flex gap-1 items-center'>
-        <span className='text-2xl font-bold bg-amber-500 py-1 rounded-lg'>
-            IMDB
-        </span>
-        <span className='text-xl hidden sm:inline'>Clone</span>
-    </Link>
+        <div className='flex items-center gap-4'>
+            <DarkModeSwitch></DarkModeSwitch>
+            <Link href={'/'} className='flex gap-1 items-center'>
+                <span className='text-2xl font-bold bg-amber-500 py-1 rounded-lg'>
+                    IMDB
+                </span>
+                <span className='text-xl hidden sm:inline'>Clone</span>
+            </Link>
+        </div>    
     </div>
   )
 }
